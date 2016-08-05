4da39046b976b80e091bd7bd68033e8d68a17b1c
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2016-08-05 20:58:40 +0300

    Add support of selectors

--- cmd/kubeac/main.go
+++ cmd/kubeac/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/Mirantis/k8s-AppController/client"
 	"github.com/Mirantis/k8s-AppController/scheduler"
 	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/labels"
 )
 
 func main() {
+	labelSelector := flag.String("l", "", "label selector")
+	flag.Parse()
+
 	var url string
-	fromEnv := os.Getenv("KUBERNETES_CLUSTER_URL")
-	if fromEnv != "" {
-		url = fromEnv
-	} else if len(os.Args) != 2 {
-		log.Fatal("Usage: k8s-ac KUBERNETES_CLUSTER_URL")
-	} else {
-		url = os.Args[1]
+	if len(flag.Args()) > 0 {
+		url = flag.Args()[0]
 	}
+	if url == "" {
+		url = os.Getenv("KUBERNETES_CLUSTER_URL")
+	}
+	if url == "" {
+		log.Fatal("Usage: kubeac [-l <label>=<value>[,<label>=<value]...] KUBERNETES_CLUSTER_URL")
+	}
+
+	sel, err := labels.Parse(*labelSelector)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Using label selector:", *labelSelector)
 
 	c, err := client.GetAppControllerClient(url)
 	if err != nil {
@@ -26,13 +39,13 @@ func main() {
 	}
 
 	log.Println("Getting resource definitions")
-	resDefList, err := c.ResourceDefinitions().List(api.ListOptions{})
+	resDefList, err := c.ResourceDefinitions().List(api.ListOptions{LabelSelector: sel})
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("Getting dependencies")
-	depList, err := c.Dependencies().List(api.ListOptions{})
+	depList, err := c.Dependencies().List(api.ListOptions{LabelSelector: sel})
 	if err != nil {
 		log.Fatal(err)
 	}
