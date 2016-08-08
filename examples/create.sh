a8f7b9891878926991b0c8679c1dd4acac46da9a
Maciej Kwiek <mkwiek@mirantis.com>
2016-08-08 15:21:43 +0200

    Adjust demo stuff for new image

--- examples/create.sh
+++ examples/create.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-kubectl.sh create -f ../appcontroller-wrap.yaml
+kubectl.sh create -f ../appcontroller.yaml
 kubectl.sh create -f ../manifests/dependencies.yaml
 kubectl.sh create -f ../manifests/resdefs.yaml
 
@@ -10,5 +10,7 @@ sleep 10
 
 kubectl.sh create -f deps.yaml
 
-cat job.yaml | kubectl.sh exec -i k8s-appcontroller-wrap wrap job1 | kubectl.sh create -f -
-cat pod.yaml | kubectl.sh exec -i k8s-appcontroller-wrap wrap pod1 | kubectl.sh create -f -
+cat job.yaml | kubectl.sh exec -i k8s-appcontroller wrap job1 | kubectl.sh create -f -
+cat pod.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod1 | kubectl.sh create -f -
+
+kubectl.sh exec k8s-appcontroller ac-run
