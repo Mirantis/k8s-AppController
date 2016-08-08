579a792e97113cdb10dbba9f6e7666c5142f8787
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2016-08-08 16:30:43 +0300

    Refactor resources 'pod' and 'job'

--- resources/job.go
+++ resources/job.go
@@ -8,43 +8,36 @@ import (
 	"k8s.io/kubernetes/pkg/client/unversioned"
 )
 
-type byLastProbeTime []batch.JobCondition
-
-func (b byLastProbeTime) Len() int {
-	return len(b)
-}
-
-func (b byLastProbeTime) Swap(i, j int) {
-	b[i], b[j] = b[j], b[i]
-}
-
-func (b byLastProbeTime) Less(i, j int) bool {
-	return b[i].LastProbeTime.After(b[j].LastProbeTime.Time)
-}
-
 type Job struct {
 	Job    *batch.Job
 	Client unversioned.JobInterface
 }
 
-func (s Job) Key() string {
-	return "job/" + s.Job.Name
+func jobKey(name string) string {
+	return "job/" + name
 }
 
-func (s Job) Status() (string, error) {
-	job, err := s.Client.Get(s.Job.Name)
+func jobStatus(j unversioned.JobInterface, name string) (string, error) {
+	job, err := j.Get(name)
 	if err != nil {
 		return "error", err
 	}
-	s.Job = job
 
-	for _, cond := range s.Job.Status.Conditions {
+	for _, cond := range job.Status.Conditions {
 		if cond.Type == "Complete" && cond.Status == "True" {
 			return "ready", nil
 		}
 	}
 
 	return "not ready", nil
+}
+
+func (s Job) Key() string {
+	return jobKey(s.Job.Name)
+}
+
+func (s Job) Status() (string, error) {
+	return jobStatus(s.Client, s.Job.Name)
 }
 
 func (s Job) Create() error {
@@ -72,22 +65,11 @@ type ExistingJob struct {
 }
 
 func (s ExistingJob) Key() string {
-	return "job/" + s.Name
+	return jobKey(s.Name)
 }
 
 func (s ExistingJob) Status() (string, error) {
-	job, err := s.Client.Get(s.Name)
-	if err != nil {
-		return "error", err
-	}
-
-	for _, cond := range job.Status.Conditions {
-		if cond.Type == "Complete" && cond.Status == "True" {
-			return "ready", nil
-		}
-	}
-
-	return "not ready", nil
+	return jobStatus(s.Client, s.Name)
 }
 
 func (s ExistingJob) Create() error {
