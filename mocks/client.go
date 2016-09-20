6642c23af978623cb04a45f759e560c372b8e77e
pprokop <piotr.prokop@intel.com>
2016-09-20 10:59:03 +0200

    Adding comment for exported funtion

--- mocks/client.go
+++ mocks/client.go
@@ -51,6 +51,7 @@ func (c *Client) PetSets() unversioned.PetSetInterface {
 	return c.PetSetInterface
 }
 
+//DaemonSets return a DaemonSetInterface of k8s
 func (c *Client) DaemonSets() unversioned.DaemonSetInterface {
 	return c.DaemonSetInterface
 }
