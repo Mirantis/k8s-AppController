64eb7c76b1dff1f8135c04e00e8ad647e91105b6
Jedrzej Nowak <pigmej@gmail.com>
2016-12-01 15:54:25 +0100

    Detect name of kubectl (kubectl vs kubectl.sh)

---
+++ examples/common.sh
@@ -0,0 +1,8 @@
+if ! [ -z $KUBECTL_NAME ]; then
+    if [ -x "$(command -v kubectl)" ]; then
+        KUBECTL_NAME='kubectl'
+    else
+        KUBECTL_NAME='$KUBECTL_NAME'
+    fi
+    export KUBECTL_NAME
+fi
--- examples/extended/delete.sh
+++ examples/extended/delete.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
+source ../common.sh
 
-kubectl.sh delete -f existing_job.yaml
+$KUBECTL_NAME delete -f existing_job.yaml
 
-kubectl.sh delete -f deps.yaml
+$KUBECTL_NAME delete -f deps.yaml
 
-cat job.yaml | kubectl.sh exec -i k8s-appcontroller wrap job1 | kubectl.sh delete -f -
-cat job2.yaml | kubectl.sh exec -i k8s-appcontroller wrap job2 | kubectl.sh delete -f -
-cat job3.yaml | kubectl.sh exec -i k8s-appcontroller wrap job3 | kubectl.sh delete -f -
-cat job4.yaml | kubectl.sh exec -i k8s-appcontroller wrap job4 | kubectl.sh delete -f -
+cat job.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap job1 | $KUBECTL_NAME delete -f -
+cat job2.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap job2 | $KUBECTL_NAME delete -f -
+cat job3.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap job3 | $KUBECTL_NAME delete -f -
+cat job4.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap job4 | $KUBECTL_NAME delete -f -
 
-cat pod.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod1 | kubectl.sh delete -f -
-cat pod2.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod2 | kubectl.sh delete -f -
-cat pod3.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod3 | kubectl.sh delete -f -
-cat pod4.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod4 | kubectl.sh delete -f -
-cat pod5.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod5 | kubectl.sh delete -f -
-cat pod6.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod6 | kubectl.sh delete -f -
-cat pod7.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod7 | kubectl.sh delete -f -
-cat pod8.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod8 | kubectl.sh delete -f -
-cat pod9.yaml | kubectl.sh exec -i k8s-appcontroller wrap pod9 | kubectl.sh delete -f -
+cat pod.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod1 | $KUBECTL_NAME delete -f -
+cat pod2.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod2 | $KUBECTL_NAME delete -f -
+cat pod3.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod3 | $KUBECTL_NAME delete -f -
+cat pod4.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod4 | $KUBECTL_NAME delete -f -
+cat pod5.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod5 | $KUBECTL_NAME delete -f -
+cat pod6.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod6 | $KUBECTL_NAME delete -f -
+cat pod7.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod7 | $KUBECTL_NAME delete -f -
+cat pod8.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod8 | $KUBECTL_NAME delete -f -
+cat pod9.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap pod9 | $KUBECTL_NAME delete -f -
 
-cat replicaset.yaml | kubectl.sh exec -i k8s-appcontroller wrap frontend | kubectl.sh delete -f -
+cat replicaset.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap frontend | $KUBECTL_NAME delete -f -
 
-cat daemonset.yaml | kubectl.sh exec -i k8s-appcontroller wrap daemonset | kubectl.sh delete -f -
+cat daemonset.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap daemonset | $KUBECTL_NAME delete -f -
 
-cat secret.yaml | kubectl.sh exec -i k8s-appcontroller wrap daemonset | kubectl.sh delete -f -
+cat secret.yaml | $KUBECTL_NAME exec -i k8s-appcontroller wrap daemonset | $KUBECTL_NAME delete -f -
 
-kubectl.sh delete -f ../../manifests/appcontroller.yaml
+$KUBECTL_NAME delete -f ../../manifests/appcontroller.yaml
