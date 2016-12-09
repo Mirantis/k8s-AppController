dd8fa356a82d6fe558c13f821f012a52d18a2c0b
Illia Khudoshyn <ikhudoshyn@mirantis.com>
2016-12-09 12:36:44 +0200

    Fix examples

--- examples/common.sh
+++ examples/common.sh
@@ -2,7 +2,7 @@ if ! [ -z $KUBECTL_NAME ]; then
     if [ -x "$(command -v kubectl)" ]; then
         KUBECTL_NAME='kubectl'
     else
-        KUBECTL_NAME='$KUBECTL_NAME'
+        KUBECTL_NAME=${KUBECTL_NAME}
     fi
     export KUBECTL_NAME
 fi
