15bb89bbc697c430e3b5e5f7174377d6795ebada
Dmitry Shulyak <yashulyak@gmail.com>
2017-02-02 09:49:23 +0200

    Publish to docker after succesfull build
    After this patch we will publish docker images after each succesfull build.
    
    We are running builds both for kubernetes 1.5.2/go1.7 and kubernetes 1.4.8/go1.6
    and image will be pushed after any succesfull build. To avoid ambiguity we will enable
    publishing only for build with go1.7.
    
    We will publish image in next cases:
    - After build will pass on master branch, we will publish image with tag - latest
    - After build will pass on tagged commit, we will publish image with specified tag
    - After build will pas on any other branch, we will publish image with name of that branch
    
    We won't be publishing images for each PR

---
+++ scripts/docker_publish.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+
+IMAGE_REPO=${IMAGE_REPO:-mirantis/k8s-appcontroller}
+PUBLISH=${PUBLISH:-}
+
+function push-to-docker {
+    if [ -z "$PUBLISH" ]; then
+        echo "Publish is disabled"
+        exit 0
+    fi
+    if [ "$TRAVIS_PULL_REQUEST_BRANCH" != "" ]; then
+         echo "Processing PR $TRAVIS_PULL_REQUEST_BRANCH"
+         exit 0
+    fi
+
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    
+    if [ ! -z "$TRAVIS_TAG" ]; then
+        echo "Pushing with tag - $TRAVIS_TAG"
+        docker push $IMAGE_REPO:$TRAVIS_TAG
+        exit 0
+    fi
+    if [ $TRAVIS_BRANCH == "master" ]; then
+        echo "Pushing with tag - latest"
+        docker push $IMAGE_REPO:latest;
+    fi
+    echo "Pushing with tag - $TRAVIS_BRANCH"
+    docker push $IMAGE_REPO:$TRAVIS_BRANCH
+}
+
+push-to-docker
+
