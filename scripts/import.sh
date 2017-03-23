98cb3e45775a04ed0d1536a7122816d3c14f1063
Alexander Arzhanov <aarzhanov@mirantis.com>
2017-03-23 19:46:41 +0300

    Fixed docker publish
    - Add copyright notices
    
    - Currently, CI pushing images only with latest tag:
      https://travis-ci.org/Mirantis/k8s-AppController/jobs/203410745#L1998-L2001
      No one paid attention to this, because the "after_success" step failure
      doesn't fail build:
      https://github.com/travis-ci/travis-ci/issues/758
      And at the moment a good fix is to use this as the last entry of
      "script" step instead of "after_success" step.

--- scripts/import.sh
+++ scripts/import.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# Copyright 2017 Mirantis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+set -o xtrace
+set -o pipefail
 set -o errexit
 set -o nounset
-set -o pipefail
-set -o xtrace
+
 
 IMAGE_REPO=${IMAGE_REPO:-mirantis/k8s-appcontroller}
 IMAGE_TAG=${IMAGE_TAG:-latest}
@@ -14,20 +28,18 @@ SLAVE_PATTERN=${SLAVE_PATTERN:-"kube-node-"}
 
 
 function import-image {
-    docker save ${IMAGE_REPO}:${IMAGE_TAG} -o "${TMP_IMAGE_PATH}"
+  docker save -o "${TMP_IMAGE_PATH}" "${IMAGE_REPO}":"${IMAGE_TAG}"
 
-    if [ -n "$MASTER_NAME" ]; then
-        docker cp "${TMP_IMAGE_PATH}" kube-master:/image.tar
-        docker exec -ti "${MASTER_NAME}" docker load -i /image.tar
-    fi
+  if [ -n "${MASTER_NAME}" ]; then
+    docker cp "${TMP_IMAGE_PATH}" "${MASTER_NAME}":/image.tar
+    docker exec -ti "${MASTER_NAME}" docker load -i /image.tar
+  fi
 
-    for i in `seq 1 "${NUM_NODES}"`;
-    do
-        docker cp "${TMP_IMAGE_PATH}" "${SLAVE_PATTERN}$i":/image.tar
-        docker exec -ti "${SLAVE_PATTERN}$i" docker load -i /image.tar
-    done
-    set +o xtrace
-    echo "Finished copying docker image to dind nodes"
+  for node in $(seq 1 "${NUM_NODES}"); do
+    docker cp "${TMP_IMAGE_PATH}" "${SLAVE_PATTERN}""${node}":/image.tar
+    docker exec -ti "${SLAVE_PATTERN}""${node}" docker load -i /image.tar
+  done
+  echo "Finished copying docker image to dind nodes"
 }
 
 import-image
